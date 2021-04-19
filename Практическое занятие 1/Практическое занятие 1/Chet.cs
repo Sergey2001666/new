a92930eac1932c4ddcb030cfbc5a6f90ed68ae26
Sergey2001666 <BigHead200107@gmail.com>
2021-04-19 12:08:55 +0300

    New

---
+++ Практическое занятие 1/Практическое занятие 1/Chet.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Практическое_занятие_1
+{
+    class Chet
+    {
+        static Chet[] a = new Chet[1000];
+        static int n;
+
+        public string Name { get; set; }
+        public int Number { get; set; }
+        public double Sum { get; set; }
+
+        internal void add()
+        {
+            Console.WriteLine();
+            Console.Write("Введите кол-во записей: ");
+            n = Convert.ToInt16(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                a[i] = new Chet();
+                Console.Write("Название счета: ");
+                a[i].Name = Console.ReadLine();
+                Console.Write("Номер счета: ");
+                a[i].Number = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Сумма: ");
+                a[i].Sum = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine();
+            }
+        }
+
+        internal void print()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Список счетов:");
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Название счета:" + a[i].Name);
+                Console.WriteLine("Номер счета:" + a[i].Number);
+                Console.WriteLine("Сумма:" + a[i].Sum);
+
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Практическое занятие 1/Практическое занятие 1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Практическое_занятие_1
+{
+    class Program
+    {
+        static Chet az = new Chet();
+
+        static void Main(string[] args)
+        {
+            for (int z = 0; z < 10000; z++)
+            {
+                Console.WriteLine(" ------------------МЕНЮ------------------ ");
+                Console.WriteLine("| 1. Создать список.                     |");
+                Console.WriteLine("| 2. Вывести список.                     |");
+                Console.WriteLine("| 3. Очистить экран                      |");
+                Console.WriteLine("| 4. Выйти из программы.                 |");
+                Console.WriteLine(" ---------------------------------------- ");
+                Console.Write("Введите число от 1 до 4: ");
+                int x = Convert.ToInt32(Console.ReadLine());
+                switch (x)
+                {
+                    case 1: az.add(); break;
+                    case 2: az.print(); break;
+                    case 3: Console.Clear(); break;
+                    case 4: Environment.Exit(0); break;
+                    default: Console.WriteLine("ОШИБКА, введите числа от 1 до 4 !!!"); break;
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
